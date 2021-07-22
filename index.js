b4316a2d3fc9e00ec925fc0a8f66d422d11461ba
Raven <jrglomar016@gmail.com>
2021-07-22 12:54:59 +0800

    for printing modification

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ db.sequelize
 
 if(process.env.ALLOW_SYNC === "true"){
     db.sequelize
-    .sync({ force: true })
+    .sync({ alter: true })
     .then(() => 
         console.log('Done adding/updating database based on Models')
     );
